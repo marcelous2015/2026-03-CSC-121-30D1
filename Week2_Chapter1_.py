01ae46fec8dcc0e4c0330c4dabc829c30dd926ee
marcelous2015 <mwilliams493@halifaxcc.edu>
2026-09-04 16:54:06 -0400

    COMPLETION

--- Week2_Chapter1_.py
+++ Week2_Chapter1_.py
@@ -1,35 +1,120 @@
-# Setup (Commit 1 - "STRUCTURE & PSEUDOCODE")
-# Python
-
-# IMPORT STATEMENTS
-# (None)
-
-# FUNCTION DEFINITIONS
-# (None)
-
-# PROGRAM LOGIC
-# 1. Print the total minutes in a movie (2 * 60 + 45).
-# 2. Print the years since 1947 (2026 - 1947).
-# 3. Print the total cost of 7 books (7 * 35).
-
-# Implementation (Commit 2 - "MIDPOINT")
-# Python
-
-print(2 * 60 + 45)
-print(2026 - 1947)
-
-# Final Polish (Commit 3 - "COMPLETION")
-# IMPORT STATEMENTS
-# (None)
-
-# FUNCTION DEFINITIONS
-# (None)
-
-# PROGRAM LOGIC
-# 1. Print the total minutes in a movie.
-# 2. Print the years since 1947.
-# 3. Print the total cost of 7 books.
-
-print("Total minutes in movie:", 2 * 60 + 45)
-print("Years since first bug:", 2026 - 1947)
-print("Total cost of books:", 7 * 35)
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNbE6X/7ym2gysUfkIwmP09",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/marcelous2015/2026-03-CSC-121-30D1/blob/main/Week2_Chapter1_.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 11,
+      "metadata": {
+        "id": "P0HWrEcnE2TD"
+      },
+      "outputs": [],
+      "source": [
+        "#Setup (Commit 1 - \"STRUCTURE & PSEUDOCODE\")\n",
+        "#Python\n",
+        "\n",
+        "# IMPORT STATEMENTS\n",
+        "# (None)\n",
+        "\n",
+        "# FUNCTION DEFINITIONS\n",
+        "# (None)\n",
+        "\n",
+        "# PROGRAM LOGIC\n",
+        "# 1. Print the total minutes in a movie (2 * 60 + 45).\n",
+        "# 2. Print the years since 1947 (2026 - 1947).\n",
+        "# 3. Print the total cost of 7 books (7 * 35)."
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#Implementation (Commit 2 - \"MIDPOINT\")\n",
+        "#Python\n",
+        "\n",
+        "print(2 * 60 + 45)\n",
+        "print(2026 - 1947)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "3KsG8MNpGQl0",
+        "outputId": "85ac6be2-4004-46bd-fea4-342962ee2195"
+      },
+      "execution_count": 12,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "165\n",
+            "79\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#Final Polish (Commit 3 - \"COMPLETION\")\n",
+        "# IMPORT STATEMENTS\n",
+        "# (None)\n",
+        "\n",
+        "# FUNCTION DEFINITIONS\n",
+        "# (None)\n",
+        "\n",
+        "# PROGRAM LOGIC\n",
+        "# 1. Print the total minutes in a movie.\n",
+        "# 2. Print the years since 1947.\n",
+        "# 3. Print the total cost of 7 books.\n",
+        "\n",
+        "print(\"Total minutes in movie:\", 2 * 60 + 45)\n",
+        "print(\"Years since first bug:\", 2026 - 1947)\n",
+        "print(\"Total cost of books:\", 7 * 35)\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "CDbzV1TaGmnG",
+        "outputId": "0f4968cf-be15-4af7-a4b3-0808ff44747f"
+      },
+      "execution_count": 13,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Total minutes in movie: 165\n",
+            "Years since first bug: 79\n",
+            "Total cost of books: 245\n"
+          ]
+        }
+      ]
+    }
+  ]
+}

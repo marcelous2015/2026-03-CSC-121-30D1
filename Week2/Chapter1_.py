be95f8e9f428b04e8ff7883db0b6b4b54c3f8fa8
marcelous2015 <mwilliams493@halifaxcc.edu>
2026-09-04 20:09:53 -0400

    STRUCTURE & PSEUDOCODE

--- Week2/Chapter1_.py
+++ Week2/Chapter1_.py
@@ -4,7 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyNbE6X/7ym2gysUfkIwmP09",
+      "authorship_tag": "ABX9TyNjvZKRJcTWaV2fHLaPApUE",
       "include_colab_link": true
     },
     "kernelspec": {
@@ -28,15 +28,12 @@
     },
     {
       "cell_type": "code",
-      "execution_count": null,
+      "execution_count": 1,
       "metadata": {
         "id": "P0HWrEcnE2TD"
       },
       "outputs": [],
       "source": [
-        "#Setup (Commit 1 - \"STRUCTURE & PSEUDOCODE\")\n",
-        "#Python\n",
-        "\n",
         "# IMPORT STATEMENTS\n",
         "# (None)\n",
         "\n",
@@ -47,73 +44,6 @@
         "# 1. Print the total minutes in a movie (2 * 60 + 45).\n",
         "# 2. Print the years since 1947 (2026 - 1947).\n",
         "# 3. Print the total cost of 7 books (7 * 35)."
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "#Implementation (Commit 2 - \"MIDPOINT\")\n",
-        "#Python\n",
-        "\n",
-        "print(2 * 60 + 45)\n",
-        "print(2026 - 1947)"
-      ],
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "3KsG8MNpGQl0",
-        "outputId": "85ac6be2-4004-46bd-fea4-342962ee2195"
-      },
-      "execution_count": null,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "165\n",
-            "79\n"
-          ]
-        }
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "#Final Polish (Commit 3 - \"COMPLETION\")\n",
-        "# IMPORT STATEMENTS\n",
-        "# (None)\n",
-        "\n",
-        "# FUNCTION DEFINITIONS\n",
-        "# (None)\n",
-        "\n",
-        "# PROGRAM LOGIC\n",
-        "# 1. Print the total minutes in a movie.\n",
-        "# 2. Print the years since 1947.\n",
-        "# 3. Print the total cost of 7 books.\n",
-        "\n",
-        "print(\"Total minutes in movie:\", 2 * 60 + 45)\n",
-        "print(\"Years since first bug:\", 2026 - 1947)\n",
-        "print(\"Total cost of books:\", 7 * 35)\n"
-      ],
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "CDbzV1TaGmnG",
-        "outputId": "0f4968cf-be15-4af7-a4b3-0808ff44747f"
-      },
-      "execution_count": null,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "Total minutes in movie: 165\n",
-            "Years since first bug: 79\n",
-            "Total cost of books: 245\n"
-          ]
-        }
       ]
     }
   ]
